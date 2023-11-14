167db5e205bb28aed24e48dd051712a13dd9c736
suleiman-gacheru <suleiman-gacheru@ubuntu-new.myguest.virtualbox.org>
2023-11-14 19:36:45 +0300

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    # Dummy korisnik ili privremeni objekt
+    User.new(name: 'Test User')
+  end
 end
+
