dcf51878477b19ccd929f3b1c670d2db6214f1ba
suleiman-gacheru <suleiman-gacheru@ubuntu-new.myguest.virtualbox.org>
2023-11-14 19:32:44 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'recipes#index'
+
+  resources :users, only: [:index]
+  resources :recipes, only: %i[index show new create destroy] do
+    collection do
+      get :public_recipes
+      get :missing_food
+    end
+  end
+
+  resources :foods
+  resources :recipe_foods
+
+  # Add any additional routes you need below this line
 end
