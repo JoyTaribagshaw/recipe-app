db1014d4779ad91f66bcf7bbbd5a235b33da675a
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-15 18:29:58 +0000

    migrate files and generate tables

--- db/migrate/20231115175633_create_recipe_foods.rb
+++ db/migrate/20231115175633_create_recipe_foods.rb
@@ -1,7 +1,8 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :recipe_foods do |t|
-
+      t.integer :quantity
+      
       t.timestamps
     end
   end
--- db/migrate/20231115175659_add_user_ref_to_foods.rb
+++ db/migrate/20231115175659_add_user_ref_to_foods.rb
@@ -1,4 +1,5 @@
 class AddUserRefToFoods < ActiveRecord::Migration[7.1]
   def change
+    add_reference :foods, :user, null: false, foreign_key: true
   end
 end
