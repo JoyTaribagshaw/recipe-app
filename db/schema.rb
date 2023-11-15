220f7c232125d526b3e09adc9a6f617c8dd41391
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-15 18:51:03 +0000

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -65,18 +65,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_182422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "views", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_views_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
