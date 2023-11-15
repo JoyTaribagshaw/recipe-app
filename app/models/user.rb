89ff245e3efa9ebd522210a924651c988e97ec40
suleiman-gacheru <dretrevor8@gmail.com>
2023-11-15 14:47:29 +0300

    add reference to users and recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :foods
+  has_many :recipes
+
+  validates :name, presence: true
 end
+
