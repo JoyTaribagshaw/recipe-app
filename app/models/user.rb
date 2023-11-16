9b99eb081014c2b952c55ea3714a1b5b76864b92
suleiman-gacheru <dretrevor8@gmail.com>
2023-11-16 16:40:00 +0300

    update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+       # Include default devise modules. Others available are:
+       # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+       devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :foods
-  has_many :recipes
+       has_many :foods, dependent: :delete_all
+       has_many :recipes, dependent: :delete_all
 
-  validates :name, presence: true
-end
+       validates :name, presence: true
+     end
