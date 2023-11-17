7898318ede802f073a581d799b8ae5616a659220
suleiman-gacheru <dretrevor8@gmail.com>
2023-11-17 21:23:42 +0300

    change destory

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,45 +1,46 @@
-before_action :set_food, only: [:destroy]
+class FoodsController < ApplicationController
+  before_action :set_food, only: [:destroy]
 
-def index
-  @foodlist = current_user.foods
-end
-
-def new
-  @food = Food.new
-end
-
-def create
-  @food = Food.new(food_params)
-  @food.user_id = current_user.id
-
-  if @food.save
-    redirect_to foods_path, notice: 'Food was successfully created.'
-  else
-    render :new
+  def index
+    @foodlist = current_user.foods
   end
-end
 
-def destroy
-  if @food
-    if @food.destroy
-      redirect_to foods_path, notice: 'Successfully destroyed foodlist.'
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
     else
-      flash[:error] = 'Error: Food could not be destroyed.'
+      render :new
+    end
+  end
+
+  def destroy
+    if @food
+      if @food.destroy
+        redirect_to foods_path, notice: 'Successfully destroyed foodlist.'
+      else
+        flash[:error] = 'Error: Food could not be destroyed.'
+        redirect_to foods_path
+      end
+    else
+      flash[:error] = 'Error: Food not found.'
       redirect_to foods_path
     end
-  else
-    flash[:error] = 'Error: Food not found.'
-    redirect_to foods_path
   end
-end
 
-private
+  private
 
-def food_params
-  params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 
-def set_food
-  @food = Food.find_by(id: params[:id])
-end
+  def set_food
+    @food = Food.find_by(id: params[:id])
+  end
 end
